5beaacbe425d858337ead2f5fdfefc3b73713758
dybeca123 <103453892+dybeca123@users.noreply.github.com>
2022-05-12 14:23:03 -0300

    Add files via upload

---
+++ exercis.js
@@ -0,0 +1,45 @@
+/*let pensar = prompt ('Dos números entre 0 e 5. Qual estou pensando?')
+
+if (numero == 5){
+    alert('Acertou')
+}else{
+    alert('Perdeu')
+}
+
+
+//exer2
+
+let numero = prompt('Coloque a velocidade do carro');
+
+if (vel > 80){
+    let multa = (vel - 80) * 5
+    alert(`Você está acima do limite. A multa é de ${multa}`)
+}else{
+    alert('Voê está dentro do limite');
+}
+
+//exerci3
+
+var m = prompt ('Digite o valor')
+var total = n%2
+if(total == 0){
+     alert ('Par')
+}else{
+    alert ('ímpar')
+}
+alert(total)*/
+
+
+//ex4
+
+let salario = parseInt(prompt('Qual é o seu salário?'));
+
+if (salario > 1250,00){
+  let aumento = (salario *10/100)+ salario;
+}else{
+    let aumento2 = (salario * 15/100) + salario;
+    alert (aumento2)
+}
+
+
+
